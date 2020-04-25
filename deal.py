769b4a39dfb1277d676571a640d77f3133e2676a
Kairong Jiang <cecil.ini.jiang@gmail.com>
2020-04-24 23:26:02 -0700

    added deal

---
+++ deal.py
@@ -0,0 +1,19 @@
+from random import shuffle
+
+characters = ['梅林', '派西维尔', '大头兵', '大头兵', '莫甘娜', '刺客', '奥伯伦']
+
+players = [
+    'Ju Pan',
+    'Teng Liang',
+    'zhuzhicai',
+    'Jingping He',
+    'Mingwei Li',
+    'shanmei',
+    'haomiao']
+
+shuffle(characters)
+shuffle(characters)
+shuffle(characters)
+
+print(characters)
+print(players)
